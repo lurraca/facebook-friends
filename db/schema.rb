cd7ebc15e5978683db190c35b6cebc791fa528b6
Luis Urraca <me@lurraca.com>
2015-09-19 16:34:22 +0100

    Initial authorization model implementation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150919152153) do
+ActiveRecord::Schema.define(version: 20150919153311) do
+
+  create_table "authorizations", force: :cascade do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "authorizations", ["user_id"], name: "index_authorizations_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
