3099b23176459ba8ec8dfa36e7f8199cf38f587d
Luis Urraca <me@lurraca.com>
2015-09-20 00:38:07 +0100

    Graph PORO to handle request to facebook Graph API

---
+++ app/models/facebook/graph.rb
@@ -0,0 +1,46 @@
+module Facebook
+  class Graph
+    attr_reader :auth_code
+    attr_accessor :access_token
+
+    def initialize(auth_code: '', access_token: '')
+      @auth_code = auth_code
+      @access_token = access_token
+    end
+
+    def set_access_token
+      response = HTTParty.get(access_token_url)
+      @access_token = parse_access_token(response)
+      self
+    end
+
+    def get_user_data
+      HTTParty.get(user_data_url)
+    end
+
+    def get_user_friends
+       HTTParty.get(user_friends_url)["data"].map { |friend| Friend.new(friend) }
+    end
+
+    private
+
+    def access_token_url
+      "https://graph.facebook.com/oauth/access_token?client_id=#{ENV['FB_APP_ID']}&" <<
+          "client_secret=#{ENV['FB_APP_SECRET']}&"<<
+          "redirect_uri=http://localhost:3000/login/facebook/callback&code=#{@auth_code}"
+    end
+
+    def parse_access_token(response)
+      URI.escape(response.split('&')[0].split('access_token=')[1])
+    end
+
+    def user_data_url
+      "https://graph.facebook.com/me/?fields=email,name,friends,picture&access_token=#{@access_token}"
+    end
+
+    def user_friends_url
+      "https://graph.facebook.com/me/taggable_friends?fields=email,name,picture&access_token=#{@access_token}"
+    end
+
+  end
+end
