7dc12809ce927077eb9d5e9e9e0356ba747b20dd
Luis Urraca <me@lurraca.com>
2015-09-20 01:06:50 +0100

    Sort by name and take only 10 friends

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,6 @@ class DashboardController < ApplicationController
   before_filter :authorize
   def index
     graph = Facebook::Graph.new(access_token: cookies.signed[:fb_access_token])
-    @friends = graph.get_user_friends
+    @friends = graph.get_user_friends.sort_by(&:name).take(10)
   end
 end
